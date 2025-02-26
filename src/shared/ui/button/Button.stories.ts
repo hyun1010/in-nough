d46a0c95572fcaa7c9f7cdb6c460f00a316bbc79
hyun1010 <rcohyun1010@gmail.com>
2025-02-26 23:24:24 +0900

    feat: 버튼 컴포넌트 제작

--- src/shared/ui/button/Button.stories.ts
+++ src/shared/ui/button/Button.stories.ts
@@ -1,52 +1,41 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import Button from './Button';
+import { Button } from '..';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Component/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  argTypes: {},
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
+    variant: 'primary',
     label: 'Button',
+    disabled: true,
   },
 };
 
 export const Secondary: Story = {
   args: {
+    variant: 'secondary',
     label: 'Button',
+    disabled: true,
   },
 };
 
-export const Large: Story = {
+export const Plain: Story = {
   args: {
-    size: 'large',
+    variant: 'plain',
     label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    disabled: true,
   },
 };
--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,3 +1,67 @@
-export default function Button() {
-  return <div>Button</div>;
+import { VariantType } from '@/shared/model';
+import { twAllMerge } from '@/shared/utils';
+
+export interface ButtonProps {
+  variant?: VariantType;
+  active?: boolean;
+  disabled?: boolean;
+  label?: string;
+  prefixIcon?: () => React.ReactElement;
+  suffixIcon?: () => React.ReactElement;
+  onClick?: () => void;
+  className?: string;
+}
+export default function Button({
+  variant = 'primary',
+  label,
+  onClick,
+  prefixIcon,
+  suffixIcon,
+  disabled = false,
+  active = false,
+  className,
+}: ButtonProps) {
+  const isOnlyIcon = label === undefined && (prefixIcon || !suffixIcon);
+
+  const primaryButtonClasses = `
+    bg-primary 
+    hover:bg-primary-hover 
+    active:bg-primary-active 
+    disabled:bg-primary-disabled
+  `;
+
+  const secondaryButtonClasses = `
+    bg-secondary 
+    hover:bg-secondary-hover 
+    active:bg-secondary-active 
+    disabled:bg-secondary-disabled
+  `;
+
+  const plainButtonClasses = `
+    border border-gray-150 text-gray-900 
+    hover:bg-gray-100 
+    active:bg-gray-200
+    disabled:bg-bg_disabled
+    disabled:text-text_disabled
+  `;
+
+  return (
+    <button
+      className={twAllMerge(
+        'w-80 text-white py-3 flex items-center justify-center rounded-md text-headline_2',
+        variant === 'primary' && primaryButtonClasses,
+        variant === 'secondary' && secondaryButtonClasses,
+        variant === 'plain' && plainButtonClasses,
+        isOnlyIcon && 'px-2.5 py-2.5 w-fit h-fit',
+        disabled && 'opacity-70 cursor-not-allowed',
+        className
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {prefixIcon && prefixIcon()}
+      {label && label}
+      {suffixIcon && suffixIcon()}
+    </button>
+  );
 }
