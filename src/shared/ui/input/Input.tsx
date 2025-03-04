e76fdf92cfb15a7578bebab70e745a923bce74d6
hyun1010 <rcohyun1010@gmail.com>
2025-03-04 20:27:12 +0900

    feat: input label 추가

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -3,6 +3,7 @@ import { twAllMerge } from '@/shared/utils';
 import { ForwardedRef, forwardRef, useEffect, useState } from 'react';
 
 export interface InputProps {
+  label?: string;
   value?: string;
   disabled?: boolean;
   placeholder?: string;
@@ -13,12 +14,14 @@ export interface InputProps {
   onSubmit?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   className?: string;
 }
+
 const InternalInput = forwardRef<HTMLInputElement, InputProps>(
   (props, ref: ForwardedRef<HTMLInputElement>) => {
     const {
+      label,
       value: initValue,
       disabled,
-      placeholder = '내용을 입력하세요.',
+      placeholder = '',
       maxLength,
       prefixIcon,
       suffixIcon,
@@ -28,6 +31,7 @@ const InternalInput = forwardRef<HTMLInputElement, InputProps>(
     } = props;
 
     const [value, setValue] = useState('');
+    const [isFocused, setIsFocused] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setValue(e.target.value);
@@ -44,28 +48,45 @@ const InternalInput = forwardRef<HTMLInputElement, InputProps>(
 
     return (
       <div className="relative w-full h-fit">
+        {label && (
+          <label
+            className={twAllMerge(
+              `absolute left-4 text-neutral-2 px-2 text-caption_medium transition-all duration-200`,
+              isFocused || value
+                ? '-top-2 text-primary bg-white'
+                : 'top-1/2  -translate-y-1/2 bg-transparent pointer-events-none'
+            )}
+          >
+            {label}
+          </label>
+        )}
+
         {prefixIcon && (
           <div className="absolute left-4 top-1/2 -translate-y-1/2 mr-2">
             {prefixIcon()}
           </div>
         )}
+
         <input
           ref={ref}
           value={value}
           maxLength={maxLength}
-          placeholder={placeholder}
+          placeholder={isFocused || value ? '' : placeholder} // 포커스되면 placeholder 숨김
           disabled={disabled}
           className={twAllMerge(
             `bg-neutral-3 rounded-lg outline-none
               h-12 w-80 px-6 py-3 text-headline_2 border
-            border-neutral-3 font-medium text-neutral-1 focus:border-primary`,
+            border-gray-150 text-neutral-1 focus:border-primary`,
             prefixIcon && 'pl-14',
             suffixIcon && 'pr-14',
             className
           )}
           onChange={handleChange}
           onKeyDown={handleOnKeyDown}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
+
         {suffixIcon && (
           <div className="absolute right-4 top-1/2 -translate-y-1/2 mr-2">
             {suffixIcon()}
