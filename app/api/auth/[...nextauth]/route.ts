f033ea735c301bf27fe36f1f4e77c9e958924a3b
hyun1010 <rcohyun1010@gmail.com>
2025-03-05 22:45:33 +0900

    mod: middle ware 로직 수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
 
 const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
   session: {
@@ -34,6 +39,9 @@ const authOptions = {
       session.accessToken = token.accessToken;
       return session;
     },
+  },
+  pages: {
+    signIn: '/signin',
   },
 };
 
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,56 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth(function middleware(req) {
-  const token = req.nextauth.token;
+const FALLBACK_URL = '/main';
+const withAuth = async (req: NextRequest, token: boolean) => {
+  const url = req.nextUrl.clone();
   const { pathname } = req.nextUrl;
 
-  if (pathname.startsWith('/main') && token) {
-    return NextResponse.next();
-  }
-  if (token) return NextResponse.redirect(new URL('/main', req.url));
-  else return NextResponse.redirect(new URL('/signin', req.url));
-});
+  if (!token) {
+    url.pathname = '/signin';
+    url.search = `callbackUrl=${pathname}`;
 
-export const config = { matcher: ['/main', '/signin', '/signup'] };
+    return NextResponse.redirect(url);
+  }
+};
+
+const withOutAuth = async (
+  req: NextRequest,
+  token: boolean,
+  to: string | null
+) => {
+  const url = req.nextUrl.clone();
+
+  if (token) {
+    url.pathname = to ?? FALLBACK_URL;
+    url.search = '';
+
+    return NextResponse.redirect(url);
+  }
+};
+
+const withAuthList = ['/main'];
+const withOutAuthList = ['/signin', '/signup'];
+
+export default async function middleware(req: NextRequest) {
+  const cookie = req.cookies.get('Authroization')?.value || '';
+
+  //setting Headers
+  const requestHeaders = new Headers(req.headers);
+  requestHeaders.set('Authroization', cookie);
+
+  const token = await getToken({ req });
+  const { searchParams } = req.nextUrl;
+  const callbackUrl = searchParams.get('callbackUrl');
+  const pathname = req.nextUrl.pathname;
+
+  const isWithAuth = withAuthList.includes(pathname);
+  const isWithOutAuth = withOutAuthList.includes(pathname);
+
+  if (isWithAuth) return withAuth(req, !!token);
+  if (isWithOutAuth) return withOutAuth(req, !!token, callbackUrl);
+}
+
+export const config = {
+  mathcher: [...withAuthList, ...withOutAuthList],
+};
