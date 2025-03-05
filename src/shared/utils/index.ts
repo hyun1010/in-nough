544ef355805613aaa2f635ccc343cd98af669af9
hyun1010 <rcohyun1010@gmail.com>
2025-03-05 23:13:24 +0900

    mod: 폴더 구조 수정

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,1 +1,12 @@
-export { twAllMerge } from './twMerge';
+import { extendTailwindMerge } from 'tailwind-merge';
+import * as config from '../../../tailwind.config';
+
+const FontSizeArray = Object.keys(config.default.theme.extend.fontSize);
+
+export const twAllMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': FontSizeArray.map((size) => `text-${size}`),
+    },
+  },
+});
