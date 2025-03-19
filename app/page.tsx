6d7d8934362ab842e59fda612cf164bb448c8a1c
hyun1010 <rcohyun1010@gmail.com>
2025-03-19 19:07:13 +0900

    style: 홈임시 버튼 퍼블리싱

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/shared/ui';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex items-center gap-2 w-screen h-screen justify-center">
+      <Button.Link variant="primary" href="/signin" label="로그인" />
+      <Button.Link variant="secondary" href="/signup" label="회원가입" />
+    </div>
+  );
 }
