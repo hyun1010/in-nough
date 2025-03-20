070efbd323897d51c194b1c4355f023ea566e077
hyun1010 <rcohyun1010@gmail.com>
2025-03-20 22:28:36 +0900

    style: 회원가입 form 퍼블리싱중

--- src/page/auth/SignFormPage.tsx
+++ src/page/auth/SignFormPage.tsx
@@ -2,8 +2,8 @@ import { SignForm } from '@/widget/form';
 
 export function SignFormPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-primary-100">
-      <div className="w-[700px] h-screen p-10 bg-white">
+    <div className="flex items-center justify-center min-h-[500px] h-screen bg-primary-100 p-10">
+      <div className="w-[600px] h-full rounded-xl shadow-sm p-10 bg-white">
         <h1 className="text-2xl font-bold text-center">회원가입</h1>
         <SignForm />
       </div>
--- src/shared/ui/form/FormLabel.tsx
+++ src/shared/ui/form/FormLabel.tsx
@@ -1,9 +1,12 @@
+import { IconCircleFilled } from '@tabler/icons-react';
 import { FormLabelProps } from './types';
 
 export default function FormLabel({ required, children }: FormLabelProps) {
   return (
     <label className="text-sm text-gray-700 flex items-center text-body_medium">
-      {required && <span className="mr-0.5">*</span>}
+      {required && (
+        <IconCircleFilled size={5} className="mr-0.5 fill-primary-500" />
+      )}
       {children}
     </label>
   );
