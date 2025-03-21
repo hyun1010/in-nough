398a0b81c1fbb8b436a366b5a0d65ba5e11fd6a1
ChyunKim <chkim@realsn.com>
2025-03-21 13:46:08 +0900

    style: logo height 수정

--- src/shared/ui/logo/DefaultLogo.tsx
+++ src/shared/ui/logo/DefaultLogo.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function DefaultLogo() {
   return (
-    <h1 className="text-4xl font-bold text-center text-primary-600 font-logo">
-      IN-Nough
+    <h1 className="text-4xl font-bold text-center h-16 text-primary-600 font-logo flex items-center justify-center">
+      IN-nough
     </h1>
   );
 }
