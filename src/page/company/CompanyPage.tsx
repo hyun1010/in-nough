ca3fb35ae280127cee1562c73c9caf7e74ac7dda
ChyunKim <chkim@realsn.com>
2025-03-21 15:38:38 +0900

    feat: company 관련 라우팅 세팅

--- src/page/company/CompanyPage.tsx
+++ src/page/company/CompanyPage.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { Logo } from '@/shared/ui';
+import { DividerWithText, Logo } from '@/shared/ui';
 import { EmailForm } from '@/widget/form';
+import Link from 'next/link';
 
 export function CompanyPage() {
   const handleSubmit = (form) => {
@@ -10,6 +11,13 @@ export function CompanyPage() {
     <div className="flex flex-col items-center justify-center w-full h-screen bg-white p-10">
       <Logo />
       <EmailForm onSubmit={handleSubmit} />
+      <DividerWithText text="또는" />
+      <Link
+        href={'/company/register'}
+        className="bg-purple-100 text-purple-900 text-sm px-3 py-1 rounded-full hover:underline"
+      >
+        아직 등록된 기업이 없나요? 기업을 등록해보세요.
+      </Link>
     </div>
   );
 }
--- src/shared/ui/logo/DefaultLogo.tsx
+++ src/shared/ui/logo/DefaultLogo.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function DefaultLogo() {
   return (
-    <h1 className="text-4xl font-bold text-center h-16 text-primary-600 font-logo flex items-center justify-center">
+    <Link
+      href={'/'}
+      className="text-4xl font-bold text-center h-16 text-primary-600 font-logo flex items-center justify-center"
+    >
       IN-nough
-    </h1>
+    </Link>
   );
 }
