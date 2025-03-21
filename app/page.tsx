d29c5492d3e6ec351d94fcc79f5bd71f10a8060f
hyun1010 <rcohyun1010@gmail.com>
2025-03-21 18:29:46 +0900

    mod: 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import { COMPANY_PATH, PATH_NAME } from '@/shared/model';
 import { Button } from '@/shared/ui';
 
 export default function Home() {
   return (
     <div className="flex items-center gap-2 w-screen h-screen justify-center">
-      <Button.Link variant="primary" href="/login" label="로그인" />
-      <Button.Link variant="secondary" href="/signup" label="회원가입" />
+      <Button.Link variant="plain" href={PATH_NAME.AUTH.LOGIN} label="로그인" />
+      <Button.Link
+        variant="secondary"
+        href={PATH_NAME.AUTH.SIGNUP}
+        label="회원가입"
+      />
+      <Button.Link
+        variant="primary"
+        href={PATH_NAME.COMPANY(COMPANY_PATH.ROOT)}
+        label="기업으로 가입"
+      />
     </div>
   );
 }
