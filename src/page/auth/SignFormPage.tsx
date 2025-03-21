b099e60d9008183aa157931d451d5410e05afe77
ChyunKim <chkim@realsn.com>
2025-03-21 15:11:38 +0900

    mod: email form 사용

--- src/page/auth/SignFormPage.tsx
+++ src/page/auth/SignFormPage.tsx
@@ -1,13 +1,15 @@
+'use client';
 import { Logo } from '@/shared/ui';
-import { SignForm } from '@/widget/form';
+import { EmailForm } from '@/widget/form';
 
 export function SignFormPage() {
+  const handleSubmit = (form) => {
+    console.log(form);
+  };
   return (
-    <div className="flex items-center justify-center min-h-[500px] h-screen bg-white p-10">
-      <div className="w-[600px] h-full rounded-xl shadow-sm p-10 bg-white">
-        <Logo />
-        <SignForm />
-      </div>
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-white p-10">
+      <Logo />
+      <EmailForm onSubmit={handleSubmit} />
     </div>
   );
 }
