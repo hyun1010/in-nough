1c8d94b8e07fa77791011262c1e886ffae883483
ChyunKim <chkim@realsn.com>
2025-03-21 13:03:42 +0900

    mod: 로고 컴포넌트로 변경

--- src/page/auth/SignFormPage.tsx
+++ src/page/auth/SignFormPage.tsx
@@ -1,10 +1,11 @@
+import { Logo } from '@/shared/ui';
 import { SignForm } from '@/widget/form';
 
 export function SignFormPage() {
   return (
-    <div className="flex items-center justify-center min-h-[500px] h-screen bg-primary-100 p-10">
+    <div className="flex items-center justify-center min-h-[500px] h-screen bg-white p-10">
       <div className="w-[600px] h-full rounded-xl shadow-sm p-10 bg-white">
-        <h1 className="text-2xl font-bold text-center">회원가입</h1>
+        <Logo />
         <SignForm />
       </div>
     </div>
