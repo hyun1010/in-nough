b92c8b04f1d6ae6a4306cec84ff92e0dc4c10bfc
ChyunKim <chkim@realsn.com>
2025-03-25 13:24:32 +0900

    style: 화면 축소시 height 깨지지 않도록 수정

--- src/widget/form/FormLayout.tsx
+++ src/widget/form/FormLayout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 
 export function FormLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen bg-white p-10">
+    <div className="flex h-screen min-h-fit w-full flex-col items-center justify-center bg-white p-10">
       <Logo />
       {children}
     </div>
