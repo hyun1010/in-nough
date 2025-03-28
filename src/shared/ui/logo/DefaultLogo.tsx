2513a72053c2550e848f39986b0542e55fde9ee3
ChyunKim <chkim@realsn.com>
2025-03-28 15:01:02 +0900

    mod: form layout 로고 눌러도 페이지 전환없도록 수정

--- src/shared/ui/logo/DefaultLogo.tsx
+++ src/shared/ui/logo/DefaultLogo.tsx
@@ -1,12 +1,28 @@
+'use client';
 import { PATH_NAME } from '@/shared/model';
+import { cn } from '@/shared/utils';
 import Link from 'next/link';
 import React from 'react';
 
-export default function DefaultLogo() {
+interface DefaultLogoProps {
+  prevent?: boolean;
+}
+
+export default function DefaultLogo({ prevent }: DefaultLogoProps) {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (prevent) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <Link
       href={PATH_NAME.HOME}
-      className="text-4xl font-bold text-center h-16 text-primary-600 font-logo flex items-center justify-center"
+      className={cn(
+        'flex h-16 items-center justify-center text-center font-logo text-4xl font-bold text-primary-600',
+        { 'cursor-default': prevent }
+      )}
+      onClick={handleClick} // 클릭 시 handleClick을 실행
     >
       IN-nough
     </Link>
--- src/widget/form/FormLayout.tsx
+++ src/widget/form/FormLayout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 export function FormLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen min-h-fit w-full flex-col items-center justify-center bg-white p-10">
-      <Logo />
+      <Logo prevent />
       {children}
     </div>
   );
